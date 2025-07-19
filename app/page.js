86712528e579adeb87844cfc49cc2d550e2064a8
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2025-07-19 19:24:12 +1000

    a character

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Practice from "./components/Practice";
 export default function Home() {
   return (
     <div className="w-screen flex flex-col items-center h-fit">
-      <h1 className="text-3xl uppercase m-4 text-center">What's the frequency, Benneth?</h1>
+      <h1 className="text-3xl uppercase m-4 text-center">Whats the frequency, Benneth?</h1>
       <Practice />
     </div>
   );
